2adf5f1ef505de14a5c698dede80dc63c47a1f80
Raymond goo <raymond.goo124@gmail.com>
2024-10-07 20:52:44 +0800

    db: init duct tape db solution 😆

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
+import json
+
 from flask import Flask, redirect, url_for, request, render_template, make_response
 
 app = Flask(__name__)
 
-@app.route('/')# #methods=['GET', 'POST'])
-def index():
+with open("db.json", "r") as file:
+    db = json.load(file)
+
+
+@app.route('/', methods=["GET", "POST"])  # #methods=['GET', 'POST'])
+def home():
     ## TODO: POST scores
-    return render_template('index.html') # GET request
+    if request.method == "GET":
+        return render_template('index.html')  # GET request
+    if request.method == "POST":
+        return json.dumps({})
 
 @app.route('/leaderboard')
 def leaderboard():
     ## TODO: Get scores
-    return render_template('leaderboard.html') 
+    return render_template('leaderboard.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    with open("db.json", "w") as file:
+        file.write(json.dumps(db))
