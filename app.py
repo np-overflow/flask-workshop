258a8939c91a95eb6214fd836c21847e61a73577
-S <npshunxiang@gmail.com>
2024-10-08 21:26:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, redirect, url_for, request, render_template, make_respo
 
 app = Flask(__name__)
 
-with open("database.json", "r") as file:
+with open("database.example.json", "r") as file:
     db = json.load(file)
 
 def get_random_topic():
@@ -32,5 +32,5 @@ if __name__ == "__main__":
     app.run(debug=True)
 
     # app.run is thread blocking and hence, the following code will only run on exit.
-    with open("database.json", "w") as file:
+    with open("database.example.json", "w") as file:
         file.write(json.dumps(db, indent=2))
