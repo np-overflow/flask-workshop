c9762325c03cc5f523015e2a26fd7822570a5748
Raymond goo <raymond.goo124@gmail.com>
2024-10-14 21:08:29 +0800

    feat: remove helper function

--- app.py
+++ app.py
@@ -17,26 +17,10 @@ def save_db():
 atexit.register(save_db)
 
 def get_random_choices():
-    if not db:
-        raise Exception("Database not initialized yet")
-    if len(db["choices"]) < 2:
-        raise Exception("Please add more than 2 choices")
-    choice1 = random.choice(db["choices"])
-    choice2 = random.choice(db["choices"])
-    while choice2 == choice1:
-        choice2 = random.choice(db["choices"])
-    return choice1, choice2
+    pass
 
 def find_choice(choice_id):
-    if not db:
-        raise Exception("Database not initialized yet")
-    for choice in db["choices"]:
-        print(choice)
-        if choice["id"] == choice_id:
-            return choice
-    else:
-        return None # we don't want to raise an exception here and crash the server.
-
+    pass
 
 ## ROUTING ##
 @app.route('/', methods=["GET", "POST"]) #VOTING PAGE
